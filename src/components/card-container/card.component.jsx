aea0452a8927350d15112582ea07bb95eb454bb8
ShereenLiao <xl3781@nyu.edu>
2022-08-11 12:10:31 -0400

    upload files

---
+++ src/components/card-container/card.component.jsx
@@ -0,0 +1,35 @@
+// import { Component } from "react";
+import "./cart.style.css";
+
+const Card = ({monster}) => {
+  const {name, id , email} = monster;
+  
+  return (
+    <div className="card-container">
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+// class Card extends Component {
+//   render() {
+//     const { name, email, id } = this.props.monster;
+//     return (
+//       <div className="card-container">
+//         <img
+//           alt={`monster ${name}`}
+//           src={`https://robohash.org/${id}?set=set2&size=180x180`}
+//         />
+//         <h2>{name}</h2>
+//         <p>{email}</p>
+//       </div>
+//     );
+//   }
+// }
+
+export default Card;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,14 @@
+import "./search-box.style.css";
+
+const SearchBox = ({className, onChangeHandler, placeholder}) => {
+  return (
+    <input
+      className={`search-box ${className}`}
+      type="search"
+      placeholder={`${placeholder}`}
+      onChange={onChangeHandler}
+    />
+  );
+};
+
+export default SearchBox;
